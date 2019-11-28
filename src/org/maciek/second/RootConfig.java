d5e83b5f3ee095133027087fc7735f502fd65b1a
Maciej Bulanda <bma@post.pl>
2019-11-28 09:14:10 +0100

    Initial commit

---
+++ src/org/maciek/second/RootConfig.java
@@ -0,0 +1,14 @@
+package org.maciek.second;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = {"org.maciek.second"}, excludeFilters = {
+        @Filter(type= FilterType.ANNOTATION, value = EnableWebMvc.class)
+})
+public class RootConfig {
+}
